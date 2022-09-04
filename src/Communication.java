be9b4eee337ac6c2c6da08df683da97b83acf117
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-09-04 12:44:36 +0300

    Рефакторинг

--- src/Communication.java
+++ src/Communication.java
@@ -2,29 +2,29 @@ import java.util.List;
 
 public interface Communication {
 
-    default double costOfItem(Item item){
+    default double costOfItem(Item item) {
         return item.getCost();
     }
 
-    default void isItemsInStorage(Seller seller, List<Item> wishListOfConsumer){
+    default void isItemsInStorage(Seller seller, List<Item> wishListOfConsumer) {
         for (Item item : wishListOfConsumer) {
-            if(!seller.getItems().contains(item)){
-                System.out.println("У нас нет " + item.getNameOfItem() );
+            if (!seller.getItems().contains(item)) {
+                System.out.println("У нас нет " + item.getNameOfItem());
             }
         }
     }
 
     //Дописать метод
-    default double totalCost(List<Item> basketAmount){
-        double summ =0;
+    default double totalCost(List<Item> basketAmount) {
+        double summ = 0;
         for (Item item : basketAmount) {
             summ += item.getCost();
         }
         return summ;
     }
 
-    default void areYouPoor (Consumer consumer ){
-        if(totalCost(consumer.wishList) > consumer.getMoney()){
+    default void areYouPoor(Consumer consumer) {
+        if (totalCost(consumer.wishList) > consumer.getMoney()) {
             System.out.println("Ты нищеброд");
         } else {
             walkInStorage();
